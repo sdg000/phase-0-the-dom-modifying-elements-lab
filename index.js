9b382aef86ffabeaeb1b01cf42053f52d4ea0c48
Seth David Gyimah <esec26@gmail.com>
2022-06-12 23:07:56 +0000

    phase 0 the dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+let node = document.getElementById("main");
+if (node.parentNode) {
+    node.parentNode.removeChild(node);
+}
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
